c2e2d9216afd1349e51c25757b99c3358b77fe7a
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-25 14:12:01 +0200

    Fix commented commands globs

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,14 @@ rm -rf "$OUTPUT_DIR"
 mkdir -p "$STATIC_BUILD"
 
 mkdir -p "$STATIC_BUILD/img"
-#cp -r "$STATIC_SRC/img/*" "$STATIC_BUILD/img"
+#cp -r "$STATIC_SRC/img"/* "$STATIC_BUILD/img"
 
 mkdir -p "$STATIC_BUILD/js"
-#cp -r "$STATIC_SRC/js/*" "$STATIC_BUILD/js"
+# cp -r "$STATIC_SRC/js"/* "$STATIC_BUILD/js"
 cp $BASEDIR/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js $STATIC_BUILD/js/bootstrap.min.js
 
 mkdir -p "$STATIC_BUILD/css"
-#cp -r "$STATIC_SRC/css/*" "$STATIC_BUILD/css"
+#cp -r "$STATIC_SRC/css"/* "$STATIC_BUILD/css"
 cp -r "$BASEDIR/node_modules/@fortawesome/fontawesome-free/css/all.min.css" "$STATIC_BUILD/css/font-awesome.css"
 hugo gen chromastyles --style=monokai > "$STATIC_BUILD/css/highlight.css"
 
