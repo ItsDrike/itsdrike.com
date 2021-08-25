84e1c7a4f4613453c06b53be8cc324d0d71e9097
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-25 10:43:25 +0200

    Create the STATIC_BUILD dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ source ./scripts/common.sh
 rm -rf "$STATIC_BUILD"
 rm -rf "$OUTPUT_DIR"
 
+mkdir -p "$STATIC_BUILD"
+
 cp -r "$STATIC_SRC/img" "$STATIC_BUILD/img"
 cp -r "$STATIC_SRC/js" "$STATIC_BUILD/js"
 cp -r "$STATIC_SRC/css" "$STATIC_BUILD/css"
