3610b7a8d970c649cae03edc7a47f801446d66e1
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-25 11:17:41 +0200

    Fix globbing expansion for static/src/scss/*

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,19 +6,19 @@ rm -rf "$OUTPUT_DIR"
 
 mkdir -p "$STATIC_BUILD"
 
-mkdir -p "$STATIC_SRC/img"
-cp -r "$STATIC_SRC/img/*" "$STATIC_BUILD/img"
+mkdir -p "$STATIC_BUILD/img"
+#cp -r "$STATIC_SRC/img/*" "$STATIC_BUILD/img"
 
-mkdir -p "$STATIC_SRC/js"
-cp -r "$STATIC_SRC/js/*" "$STATIC_BUILD/js"
+mkdir -p "$STATIC_BUILD/js"
+#cp -r "$STATIC_SRC/js/*" "$STATIC_BUILD/js"
 
-mkdir -p "$STATIC_SRC/css"
-cp -r "$STATIC_SRC/css/*" "$STATIC_BUILD/css"
+mkdir -p "$STATIC_BUILD/css"
+#cp -r "$STATIC_SRC/css/*" "$STATIC_BUILD/css"
 cp -r "$BASEDIR/node_modules/@fortawesome/fontawesome-free/css/all.min.css" "$STATIC_BUILD/css/font-awesome.css"
 hugo gen chromastyles --style=monokai > "$STATIC_BUILD/css/highlight.css"
 
-mkdir -p "$STATIC_SRC/scss"
-cp -r "$STATIC_SRC/scss/*" "$STATIC_BUILD/scss"
+mkdir -p "$STATIC_BUILD/scss"
+cp -r "$STATIC_SRC/scss"/* "$STATIC_BUILD/scss"
 
 
 hugo -vDEF --gc $@
